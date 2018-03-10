d49e8f7bf44745b93fb876eb56bbcc83bdab97e9
Aleksandar Kex Trpeski <kex@pristap.com>
2018-03-10 03:05:35 +0100

    Upgraded Elchemy & finished the article.

--- lib/Hello.elchemy.ex
+++ lib/Hello.elchemy.ex
@@ -1,5 +1,5 @@
 
-# Compiled using Elchemy v0.6.4
+# Compiled using Elchemy v0.6.6
 defmodule Hello do
   use Elchemy
 
@@ -22,5 +22,32 @@ defmodule Hello do
     "world!"
   end
 
-end
+  @doc """
+  Returns the sum of every integer in the function
+  
+      iex> import Hello
+      iex> Hello.sum([1, 2, 3, 4, 5])
+      15
 
+      iex> import Hello
+      iex> Hello.sum([10])
+      10
+
+      iex> import Hello
+      iex> Hello.sum([])
+      0
+
+  
+  """
+  @spec sum(list(integer)) :: integer
+  curry sum/1
+  def sum(list) do
+    case list do
+      [first | rest] ->
+        (first + sum(rest))
+      [] ->
+        0
+    end
+  end
+
+end
